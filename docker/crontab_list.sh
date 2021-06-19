59969bdd6b06bcc8e40c90231aaa36373d851a91
cjchd <12345677@qq.com>
2021-06-19 10:18:03 +0800

    crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -16,8 +16,7 @@
 #每日抽奖(活动时间：2021-05-01至2021-05-31)
 13 1,22,23 * * * node /scripts/jd_daily_lottery.js >> /scripts/logs/jd_daily_lottery.log 2>&1
 
-#手机狂欢城
-0 0,12,18,21 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
+
 #618动物联萌
 33 0,6-23/2 * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
 #金榜创造营 活动时间：2021-05-21至2021-12-31
