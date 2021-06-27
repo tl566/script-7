1615f05142472366ad76f2a7ec2c380c473fe955
DoveBoy <393366046@qq.com>
2021-06-27 08:02:46 +0800

    新增 京东到家鲜豆任务&京东到家鲜豆庄园

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -136,3 +136,7 @@
 30 8,11,17 * * * node /scripts/jd_jddj_fruit.js >> /scripts/logs/jd_jddj_fruit.log 2>&1
 #京东到家-农场收集水车水滴
 10 0-23/2 * * * node /scripts/jd_jddj_fruit_collect.js >> /scripts/logs/jd_jddj_fruit_collect.log 2>&1
+#京东到家鲜豆任务
+10 6 * * * node /scripts/jd_jddj_dailyTask.js >> /scripts/logs/jd_jddj_dailyTask.log 2>&1
+#京东到家鲜豆庄园
+10 7,10,16 * * * node /scripts/jd_jddj_plantBeans.js >> /scripts/logs/jd_jddj_plantBeans.log 2>&1
