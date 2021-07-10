69d8f359b901596a1c6e0c7af561dea98a015b42
wisz2021 <wei09mozzie@gmail.com>
2021-07-10 19:25:05 +0800

    取用户名

--- jd_joy.js
+++ jd_joy.js
@@ -66,7 +66,7 @@ const weAppUrl = 'https://draw.jdfcloud.com//pet';
       $.isLogin = true;
       $.nickName = '';
       await TotalBean();
-      console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*******\n`);
+      console.log(`\n******开始【京东账号${$.index}】${$.UserName}*******\n`);
       if (!$.isLogin) {
         $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
 
