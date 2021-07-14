68991cdaf8fe76aafb1886abda4bbe6267fc11e3
wisz2021 <wei09mozzie@gmail.com>
2021-07-14 12:27:15 +0800

    增加 京东健康社区兑换商品，默认兑换20京豆。需兑换其他请使用环境变量 HEALTH_EXCHANGE_NAME

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -120,6 +120,8 @@
 13 1,6,22 * * * node /scripts/jd_health.js >> /scripts/logs/jd_health.log 2>&1
 #京东健康社区收集健康能量
 5-45/20 * * * * node /scripts/jd_health_collect.js >> /scripts/logs/jd_health_collect.log 2>&1
+#京东健康社区兑换商品
+0 0 * * * node /scripts/jd_health_exchange.js >> /scripts/logs/jd_health_exchange.log 2>&1
 # 幸运大转盘
 10 10,23 * * * node /scripts/jd_market_lottery.js >> /scripts/logs/jd_market_lottery.log 2>&1
 # 领金贴
