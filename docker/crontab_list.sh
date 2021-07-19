6b3f97da67e41f116945042932646567b668142d
Ffc <cfff@qq.com>
2021-07-19 20:12:26 +0800

    crontab_list.sh 移除点点券

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -134,8 +134,6 @@
 15 0,12,22 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 #京喜牧场
 25 6-21/3 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
-#点点券
-10 0,1,20,21 * * * node /scripts/jd_necklace.js >> /scripts/logs/jd_necklace.log 2>&1
 #京小鸽
 30 2,20 * * * node /scripts/jd_jxg.js >> /scripts/logs/jd_jxg.log 2>&1
 #天天加速
