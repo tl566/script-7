3d1167570ba48089c7e850cfd8d8881107f47cc6
wisz2021 <wei09mozzie@gmail.com>
2021-07-23 21:30:53 +0800

    根据smiek加密修复点点券

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -134,6 +134,8 @@
 5 0 * * * node /scripts/jd_jin_tie.js >> /scripts/logs/jd_jin_tie.log 2>&1
 # 跳跳乐瓜分京豆
 15 0,12,22 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
+#点点券
+10 0,1,20,21 * * * node /scripts/jd_necklace.js >> /scripts/logs/jd_necklace.log 2>&1
 #京喜牧场
 25 6-21/3 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
 #京小鸽
