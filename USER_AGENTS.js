d305322dc90b03186e214204dd17b778eb0497d3
wisz2021 <wei09mozzie@gmail.com>
2021-07-30 12:54:17 +0800

    更新 USER_AGENTS.js

--- USER_AGENTS.js
+++ USER_AGENTS.js
@@ -44,6 +44,18 @@ const USER_AGENTS = [
 function randomNumber(min = 0, max = 100) {
   return Math.min(Math.floor(min + Math.random() * (max - min)), max);
 }
+
+/**
+ * 得到一个两数之间的随机整数，包括两个数在内
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值
+}
 /**
  * 生成随机 iPhoneID
  * @returns {string}
@@ -56,7 +68,7 @@ function randPhoneId() {
       Math.random().toString(36).slice(2, 10);
 }
 
-const USER_AGENT = `jdapp;iPhone;10.0.8;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randPhoneId()};network/4g;model/iPhone11,8;addressid/1188016812;appBuild/167724;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1`;
+const USER_AGENT = `jdapp;iPhone;10.0.10;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randPhoneId()};network/4g;model/iPhone11,8;addressid/1188016812;appBuild/167724;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS ${getRandomIntInclusive(11, 14)}_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1`;
 
 module.exports = {
   USER_AGENT
