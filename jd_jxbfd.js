e160e83b50754644455e093525038898de12f348
wisz2021 <wei09mozzie@gmail.com>
2021-08-19 17:44:07 +0800

    财富岛增加小程序端 签到

--- jd_jxbfd.js
+++ jd_jxbfd.js
@@ -1,7 +1,7 @@
 /*
 新版京喜财富岛，已完成
 cron：5 1,3-8/1,12,13,18,19 * * *
-更新日期：2021-07-25
+更新日期：2021-08-19
  */
 const $ = new Env("京喜财富岛");
 !function(n){"use strict";function t(n,t){var r=(65535&n)+(65535&t);return(n>>16)+(t>>16)+(r>>16)<<16|65535&r}function r(n,t){return n<<t|n>>>32-t}function e(n,e,o,u,c,f){return t(r(t(t(e,n),t(u,f)),c),o)}function o(n,t,r,o,u,c,f){return e(t&r|~t&o,n,t,u,c,f)}function u(n,t,r,o,u,c,f){return e(t&o|r&~o,n,t,u,c,f)}function c(n,t,r,o,u,c,f){return e(t^r^o,n,t,u,c,f)}function f(n,t,r,o,u,c,f){return e(r^(t|~o),n,t,u,c,f)}function i(n,r){n[r>>5]|=128<<r%32,n[14+(r+64>>>9<<4)]=r;var e,i,a,d,h,l=1732584193,g=-271733879,v=-1732584194,m=271733878;for(e=0;e<n.length;e+=16)i=l,a=g,d=v,h=m,g=f(g=f(g=f(g=f(g=c(g=c(g=c(g=c(g=u(g=u(g=u(g=u(g=o(g=o(g=o(g=o(g,v=o(v,m=o(m,l=o(l,g,v,m,n[e],7,-680876936),g,v,n[e+1],12,-389564586),l,g,n[e+2],17,606105819),m,l,n[e+3],22,-1044525330),v=o(v,m=o(m,l=o(l,g,v,m,n[e+4],7,-176418897),g,v,n[e+5],12,1200080426),l,g,n[e+6],17,-1473231341),m,l,n[e+7],22,-45705983),v=o(v,m=o(m,l=o(l,g,v,m,n[e+8],7,1770035416),g,v,n[e+9],12,-1958414417),l,g,n[e+10],17,-42063),m,l,n[e+11],22,-1990404162),v=o(v,m=o(m,l=o(l,g,v,m,n[e+12],7,1804603682),g,v,n[e+13],12,-40341101),l,g,n[e+14],17,-1502002290),m,l,n[e+15],22,1236535329),v=u(v,m=u(m,l=u(l,g,v,m,n[e+1],5,-165796510),g,v,n[e+6],9,-1069501632),l,g,n[e+11],14,643717713),m,l,n[e],20,-373897302),v=u(v,m=u(m,l=u(l,g,v,m,n[e+5],5,-701558691),g,v,n[e+10],9,38016083),l,g,n[e+15],14,-660478335),m,l,n[e+4],20,-405537848),v=u(v,m=u(m,l=u(l,g,v,m,n[e+9],5,568446438),g,v,n[e+14],9,-1019803690),l,g,n[e+3],14,-187363961),m,l,n[e+8],20,1163531501),v=u(v,m=u(m,l=u(l,g,v,m,n[e+13],5,-1444681467),g,v,n[e+2],9,-51403784),l,g,n[e+7],14,1735328473),m,l,n[e+12],20,-1926607734),v=c(v,m=c(m,l=c(l,g,v,m,n[e+5],4,-378558),g,v,n[e+8],11,-2022574463),l,g,n[e+11],16,1839030562),m,l,n[e+14],23,-35309556),v=c(v,m=c(m,l=c(l,g,v,m,n[e+1],4,-1530992060),g,v,n[e+4],11,1272893353),l,g,n[e+7],16,-155497632),m,l,n[e+10],23,-1094730640),v=c(v,m=c(m,l=c(l,g,v,m,n[e+13],4,681279174),g,v,n[e],11,-358537222),l,g,n[e+3],16,-722521979),m,l,n[e+6],23,76029189),v=c(v,m=c(m,l=c(l,g,v,m,n[e+9],4,-640364487),g,v,n[e+12],11,-421815835),l,g,n[e+15],16,530742520),m,l,n[e+2],23,-995338651),v=f(v,m=f(m,l=f(l,g,v,m,n[e],6,-198630844),g,v,n[e+7],10,1126891415),l,g,n[e+14],15,-1416354905),m,l,n[e+5],21,-57434055),v=f(v,m=f(m,l=f(l,g,v,m,n[e+12],6,1700485571),g,v,n[e+3],10,-1894986606),l,g,n[e+10],15,-1051523),m,l,n[e+1],21,-2054922799),v=f(v,m=f(m,l=f(l,g,v,m,n[e+8],6,1873313359),g,v,n[e+15],10,-30611744),l,g,n[e+6],15,-1560198380),m,l,n[e+13],21,1309151649),v=f(v,m=f(m,l=f(l,g,v,m,n[e+4],6,-145523070),g,v,n[e+11],10,-1120210379),l,g,n[e+2],15,718787259),m,l,n[e+9],21,-343485551),l=t(l,i),g=t(g,a),v=t(v,d),m=t(m,h);return[l,g,v,m]}function a(n){var t,r="",e=32*n.length;for(t=0;t<e;t+=8)r+=String.fromCharCode(n[t>>5]>>>t%32&255);return r}function d(n){var t,r=[];for(r[(n.length>>2)-1]=void 0,t=0;t<r.length;t+=1)r[t]=0;var e=8*n.length;for(t=0;t<e;t+=8)r[t>>5]|=(255&n.charCodeAt(t/8))<<t%32;return r}function h(n){return a(i(d(n),8*n.length))}function l(n,t){var r,e,o=d(n),u=[],c=[];for(u[15]=c[15]=void 0,o.length>16&&(o=i(o,8*n.length)),r=0;r<16;r+=1)u[r]=909522486^o[r],c[r]=1549556828^o[r];return e=i(u.concat(d(t)),512+8*t.length),a(i(c.concat(e),640))}function g(n){var t,r,e="";for(r=0;r<n.length;r+=1)t=n.charCodeAt(r),e+="0123456789abcdef".charAt(t>>>4&15)+"0123456789abcdef".charAt(15&t);return e}function v(n){return unescape(encodeURIComponent(n))}function m(n){return h(v(n))}function p(n){return g(m(n))}function s(n,t){return l(v(n),v(t))}function C(n,t){return g(s(n,t))}function A(n,t,r){return t?r?s(t,n):C(t,n):r?m(n):p(n)}$.md5=A}(this);
@@ -930,10 +930,11 @@ async function doTasks() {
 }
 async function rewardSign() {
   await GetTakeAggrPage();
+  await GetTakeAggrPage('app_user');
   if ($.TakeAggrPageData && $.TakeAggrPageData.hasOwnProperty('Sign')) {
     const { SignList, dwTodayStatus, dwTodayId } = $.TakeAggrPageData['Sign'];
     if (dwTodayStatus === 1) {
-      console.log(`\n【连续营业赢红包】 奖励已领取\n`);
+      console.log(`\nAPP端【连续营业赢红包】 奖励已领取\n`);
     } else {
       let ddwCoin = 0, ddwMoney = 0, dwPrizeType = 0, dwPrizeLv = 0, strPrizePool = "", strDiscount = '';
       for (let sign of SignList) {
@@ -946,9 +947,9 @@ async function rewardSign() {
           strPrizePool = sign['strPrizePool'] ? sign['strPrizePool'] : "";
         }
       }
-      console.log(`开始做 【连续营业赢红包】任务，可获得京币：${ddwCoin}个，红包：${strDiscount}元`);
+      console.log(`开始做 APP端【连续营业赢红包】任务，可获得京币：${ddwCoin}个，红包：${strDiscount}元`);
       const body = `ddwCoin=${ddwCoin}&ddwMoney=${ddwMoney}&dwPrizeType=${dwPrizeType}&strPrizePool=${strPrizePool}&dwPrizeLv=${dwPrizeLv}`;
-      await RewardSign(body);
+      await RewardSign(body, 'app_user');
     }
   }
   //打工赢红包
@@ -964,10 +965,10 @@ async function rewardSign() {
     }
   }
 }
-function GetTakeAggrPage() {
+function GetTakeAggrPage(type = 'xcx_user') {
   return new Promise(async (resolve) => {
-    const options = taskUrl('story/GetTakeAggrPage', ``, '_cfd_t,bizCode,dwEnv,ptag,source,strZone');
-    $.get(options, (err, resp, data) => {
+    const options = taskUrl('story/GetTakeAggrPage', ``, '_cfd_t,bizCode,dwEnv,ptag,source,strZone', type);
+    $.get(options, async (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
@@ -977,6 +978,27 @@ function GetTakeAggrPage() {
           if (data) {
             if (data['iRet'] === 0) {
               $.TakeAggrPageData = data['Data'];
+              if ($.TakeAggrPageData && $.TakeAggrPageData.hasOwnProperty('Sign') && type === 'xcx_user') {
+                const { SignList, dwTodayStatus, dwTodayId } = $.TakeAggrPageData['Sign'];
+                if (dwTodayStatus === 1) {
+                  console.log(`\n小程序端【连续营业赢红包】 奖励已领取\n`);
+                } else {
+                  let ddwCoin = 0, ddwMoney = 0, dwPrizeType = 0, dwPrizeLv = 0, strPrizePool = "", strDiscount = '';
+                  for (let sign of SignList) {
+                    if (dwTodayId === sign['dwDayId']) {
+                      ddwCoin = sign['ddwCoin'];
+                      ddwMoney = sign['ddwMoney'];
+                      dwPrizeType = sign['dwPrizeType'];
+                      dwPrizeLv = sign['dwBingoLevel'];
+                      strDiscount = sign['strDiscount'];
+                      strPrizePool = sign['strPrizePool'] ? sign['strPrizePool'] : "";
+                    }
+                  }
+                  console.log(`开始做 小程序端【连续营业赢红包】任务，可获得京币：${ddwCoin}个，红包：${strDiscount}元`);
+                  const body = `ddwCoin=${ddwCoin}&ddwMoney=${ddwMoney}&dwPrizeType=${dwPrizeType}&strPrizePool=${strPrizePool}&dwPrizeLv=${dwPrizeLv}`;
+                  await RewardSign(body, 'xcx_user');
+                }
+              }
             } else {
               console.log(`GetTakeAggrPage失败: ${data['sErrMsg']}, iRet: ${data['iRet']}\n`)
             }
@@ -990,9 +1012,9 @@ function GetTakeAggrPage() {
     })
   });
 }
-function RewardSign(body) {
+function RewardSign(body, type = 'xcx_user') {
   return new Promise(async (resolve) => {
-    const options = taskUrl('story/RewardSign', body, '_cfd_t,bizCode,ddwCoin,ddwMoney,dwEnv,dwPrizeLv,dwPrizeType,ptag,source,strPrizePool,strZone');
+    const options = taskUrl('story/RewardSign', body, '_cfd_t,bizCode,ddwCoin,ddwMoney,dwEnv,dwPrizeLv,dwPrizeType,ptag,source,strPrizePool,strZone', type);
     $.get(options, (err, resp, data) => {
       try {
         if (err) {
@@ -1002,9 +1024,9 @@ function RewardSign(body) {
           data = $.toObj(data);
           if (data) {
             if (data['iRet'] === 0) {
-              console.log(`连续营业赢红包 奖励领取成功，京币：${data['Data']['ddwCoin']}，红包：${data['Data']['strPrizeName']}\n`);
+              console.log(`${type === 'xcx_user' ? '小程序端' : 'APP端'} 连续营业赢红包 奖励领取成功，京币：${data['Data']['ddwCoin']}，红包：${data['Data']['strPrizeName']}\n`);
             } else {
-              console.log(`连续营业赢红包 奖励领取失败: ${data['sErrMsg']}, iRet: ${data['iRet']}\n`)
+              console.log(`${type === 'xcx_user' ? '小程序端' : 'APP端'} 连续营业赢红包 奖励领取失败: ${data['sErrMsg']}, iRet: ${data['iRet']}\n`)
             }
           }
         }
@@ -1366,7 +1388,7 @@ function sellgoods(body, type) {
     })
   });
 }
-function taskUrl(function_path, body = '', stk = '') {
+function taskUrl(function_path, body = '', stk = '', type = '') {
   let url = `${JD_API_HOST}/jxbfd/${function_path}?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=${Date.now()}&ptag=&${body}&_stk=${encodeURIComponent(stk)}&_ste=1`;
   if (['Award', 'DoTask'].includes(function_path)) {
     //bizCode=jxbfddch 不同
@@ -1374,6 +1396,9 @@ function taskUrl(function_path, body = '', stk = '') {
   }
   if (['ComposeGameAddProcess', 'ComposeGameAward'].includes(function_path)) {
     url = `${JD_API_HOST}/jxbfd/${function_path}?strZone=jxbfd&_t=${Date.now()}&${body}&_stk=${encodeURIComponent(stk)}&_ste=1`;
+  }
+  if (type === 'xcx_user') {
+    url = `${JD_API_HOST}/jxbfd/${function_path}?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=4&_cfd_t=${Date.now()}&ptag=&${body}&_stk=${encodeURIComponent(stk)}&_ste=1`;
   }
   url += `&h5st=${decrypt(Date.now(), stk, '', url)}&_=${Date.now() + 2}&sceneval=2&g_login_type=1&g_ty=ls`;
   return {
