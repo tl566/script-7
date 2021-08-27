02294e3108813c7efc339a79d6e7e6d2cff4a7c4
wisz2021 <wei09mozzie@gmail.com>
2021-08-27 09:24:23 +0800

    移除过期活动，818手机狂欢城

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -22,8 +22,6 @@
 0 1,22 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
 #酷跑零食街(活动时间：2021-06-20到2021-12-31)
 18 7,12,20 * * * node /scripts/jd_foodRunning.js >> /scripts/logs/jd_foodRunning.log 2>&1
-#818手机狂欢城（8.9-8.28）
-0 0-18/6,22 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 #5G超级盲盒 2021-08-2到2021-10-29
 0 0,1-23/3 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
 #星小店-肖战（8.9-8.25）
