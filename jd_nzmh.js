c33f914b15aac7e9f09ababea16a99dd170e284a
star <869728223@qq.com>
2021-09-09 22:55:51 +0800

    update

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -67,9 +67,21 @@ if ($.isNode()) {
         continue
       }
       try {
-        await jdMh('https://h5.m.jd.com/babelDiy/Zeus/3pG9h6Buegznv8rhVMzMR753pUtY/index.html')
-        await jdMh('https://h5.m.jd.com/babelDiy/Zeus/3bMo2AgbRYYfZt83qHLZ3ruVtrtG/index.html')
-        await jdMh('https://anmp.jd.com/babelDiy/Zeus/3c3Bt7WEWr28mWZKXbTR7f881WHM/index.html')
+        let nowTime = Date.now();
+        if(nowTime < 1632067199000){
+          await jdMh('https://h5.m.jd.com/babelDiy/Zeus/3tcmvheLZSeuP349ci3KTS7szJaS/index.html')  //end9-19
+        }else{
+          if($.index === 1){
+            await notify.sendNotify('女装盲盒','失效请维护');
+          }
+        }
+        if(nowTime < 1633017599000){
+          await jdMh('https://anmp.jd.com/babelDiy/Zeus/2z9yVc6pg2Rixp4zdMEGi7GBBUqP/index.html')  //end9-30
+        }else{
+          if($.index === 1){
+            await notify.sendNotify('女装盲盒','失效请维护');
+          }
+        }
       } catch (e) {
         $.logErr(e)
       }
