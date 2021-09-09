adb0ac588a10b8e8ced1d6260080d1af2da7a33a
123 <123@123.com>
2021-09-09 20:18:15 +0800

    内容鉴赏官

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -24,6 +24,8 @@
 18 7,12,20 * * * node /scripts/jd_foodRunning.js >> /scripts/logs/jd_foodRunning.log 2>&1
 #5G超级盲盒 2021-08-2到2021-10-29
 0 0,1-23/3 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
+#内容鉴赏官 2021-09-1到2021-12-31
+0 0,22 * * * node /scripts/jd_jsg.js >> /scripts/logs/jd_jsg.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
