c6f23800b49f5725d90d4dec60a1905dbb4f1ae3
123 <123@123.com>
2021-09-11 21:50:31 +0800

    crontab_list.sh修改脚本名称。解决docker v4添加定时任务失败的情况

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -31,7 +31,7 @@
 #京喜财富岛——合成月饼 2021-09-10到2021-9-22
 20 * * * * node /scripts/jd_jxbfd_pear_game.js >> /scripts/logs/jd_jxbfd_pear_game.log 2>&1
 #京东小家电签到（2021年9月3日-2021年9月30日）
-0 0,1 * * * node /scripts/jd_xjd_sign.js >> /scripts/logs/jd_xjd_sign.log 2>&1
+0 0,1 * * * node /scripts/jd_xjdSign.js >> /scripts/logs/jd_xjdSign.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
