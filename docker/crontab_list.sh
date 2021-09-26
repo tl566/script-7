daa0a3fddc55bab4cb3a060b0216778ea6d0dac3
wisz2021 <wei09mozzie@gmail.com>
2021-09-26 11:03:36 +0800

    小魔方 活动地址：https://h5.m.jd.com/babelDiy/Zeus/2umkvbpZCUtyN6gcymN88ew8WLeU/index.html 活动时间：2021年9月24日0:00 – 9月29日24:00

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -36,7 +36,8 @@
 5 0,1 * * * node /scripts/jd_flsSign.js >> /scripts/logs/jd_flsSign.log 2>&1
 #818手机狂欢城（9.16-10.1）
 0 0-18/6,22 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
-
+# 小魔方（2021年9月24日0:00 – 9月29日24:00）
+10 0,22 * * * * * * node /scripts/jd_desire.js >> /scripts/logs/jd_desire.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
