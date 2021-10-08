4c98de8038a79549f231133fbe0ab47a7bead8c3
wisz2021 <wei09mozzie@gmail.com>
2021-10-08 09:47:35 +0800

    增加揭榜拿京豆功能

--- jd_gold_creator.js
+++ jd_gold_creator.js
@@ -2,7 +2,7 @@
 金榜创造营
 活动入口：https://h5.m.jd.com/babelDiy/Zeus/2H5Ng86mUJLXToEo57qWkJkjFPxw/index.html
 活动时间：2021-05-21至2021-12-31
-脚本更新时间：2021-05-28 14:20
+脚本更新时间：2021-10-8
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ===================quantumultx================
 [task_local]
@@ -80,6 +80,7 @@ async function main() {
   try {
     await goldCreatorTab();//获取顶部主题
     await getDetail();
+    await goldCreatorPublish();
     await showMsg();
   } catch (e) {
     $.logErr(e)
@@ -101,6 +102,40 @@ async function getDetail() {
     await goldCreatorDetail(item['matGrpId'], item['subTitleId'], item['taskId'], item['batchId']);
     await $.wait(2000);
   }
+}
+function goldCreatorPublish() {
+  return new Promise(resolve => {
+    const options = taskUrl('goldCreatorPublish')
+    $.get(options, async (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} goldCreatorPublish API请求失败，请检查网路重试`)
+        } else {
+          data = $.toObj(data);
+          if (data) {
+            if (data.code === '0') {
+              if (data.result.subCode === '0') {
+                if (data.result.lotteryResult.lotteryCode === '0') {
+                  console.log(`揭榜成功：获得${data.result.lotteryResult.lotteryScore}京豆\n`)
+                } else {
+                  console.log(`揭榜成功：获得空气~\n`)
+                }
+              } else {
+                console.log(`揭榜失败：${$.toStr(data)}\n`);
+              }
+            } else {
+              console.log(`揭榜异常：${$.toStr(data)}\n`);
+            }
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
 }
 function goldCreatorTab() {
   $.subTitleInfos = [];
