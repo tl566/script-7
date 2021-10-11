45a889b5208cfc799a1b26f6eb88eee7b5b7264b
LingFeng <lingfeng_0918@163.com>
2021-10-11 10:17:50 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -102,7 +102,7 @@
 #一加
 25 14 * * * node /scripts/jd_yijia.js >> /scripts/logs/jd_yijia.log 2>&
 #东东世界兑换
-0 8 * * * node /scripts/jd_ddworld_exchange.js >> /scripts/logs/jd_ddworld_exchange.log 2>&
+0 0 * * * node /scripts/jd_ddworld_exchange.js >> /scripts/logs/jd_ddworld_exchange.log 2>&
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
