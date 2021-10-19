d1636a93907ae406f61e6a95e164bc35ef8134b7
123 <123@123.com>
2021-10-20 00:55:51 +0800

    crontab_list.sh update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -42,6 +42,8 @@
 15 4,15 * * * node /scripts/jd_tree.js >> /scripts/logs/jd_tree.log 2>&1
 # 京东小家（9.28 - 10.28）
 10 1 * * * node /scripts/jd_chinaJoy.js >> /scripts/logs/jd_chinaJoy.log 2>&1
+# 城城
+10 * * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
