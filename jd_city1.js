ef594b85826dda4e3854cd91b28c1e3b141d8e06
beijiyouyu <198593776@qq.com>
2021-10-23 14:10:58 +0800

    修复逻辑显示问题

--- jd_city1.js
+++ jd_city1.js
@@ -98,7 +98,7 @@ let inviteCodes = []
                         // break
                     }
                     if (res['data']['result']['toasts'] && res['data']['result']['toasts'][0]) {
-                        console.log(`助力 【${code['code']}】:${res.data.result.toasts[0].msg}`)
+                        console.log(`助力 【${code1['code']}】:${res.data.result.toasts[0].msg}`)
                     }
                 }
                 if ((res && res['status'] && res['status'] === '3') || (res && res.data && res.data.bizCode === -11)) {
