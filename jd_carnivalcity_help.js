d8c5153e53483ccbebcbab0d020ea8032a6acad0
LingFeng9527 <87851165+LingFeng9527@users.noreply.github.com>
2021-10-24 21:49:49 +0800

    Update jd_carnivalcity_help.js

--- jd_carnivalcity_help.js
+++ jd_carnivalcity_help.js
@@ -346,7 +346,7 @@ function taskUrl(body = {}) {
             "referer": "https://carnivalcity.m.jd.com/",
             "origin": "https://carnivalcity.m.jd.com",
             "Cookie": cookie,
-            "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('../USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
+            "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
         }
     }
 }
