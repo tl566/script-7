038d24cc26c3d5ae0af18d579f699a5960ece8eb
DoveBoy <393366046@qq.com>
2021-10-25 21:27:24 +0800

    东东小窝暂时下线

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -104,7 +104,7 @@
 # 京喜工厂
 50 * * * * node /scripts/jd_dreamFactory.js >> /scripts/logs/jd_dreamFactory.log 2>&1
 # 东东小窝
-46 6,23 * * * node /scripts/jd_small_home.js >> /scripts/logs/jd_small_home.log 2>&1
+#46 6,23 * * * node /scripts/jd_small_home.js >> /scripts/logs/jd_small_home.log 2>&1
 # 东东工厂
 26 * * * * node /scripts/jd_jdfactory.js >> /scripts/logs/jd_jdfactory.log 2>&1
 # 赚京豆(微信小程序)
