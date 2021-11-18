771c342427c87c386de0ea4d02b1edd1e6d493b8
Meagic <Meagic@gmail.com>
2021-11-18 16:03:42 +0800

    京喜牧场助农兑换

--- m_jx_mc_zn_exchange.js
+++ m_jx_mc_zn_exchange.js
@@ -4,7 +4,7 @@
 * 59 19 11-30 11 * m_jx_mc_zn_exchange.js
 * */
 const {Env} = require('./magic');
-const $ = new Env('M牧场兑换');
+const $ = new Env('M牧场助农兑换');
 $.logic = async function () {
     let {goodslist} = await GetLoveGoodsList();
     //红包商品
