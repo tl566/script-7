762c91d79c4edc048f876ea044845ae0d5f85ef4
LingFeng <lingfeng_0918@163.com>
2021-12-15 11:05:17 +0800

    add-逛京东会场

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -116,6 +116,8 @@
 30 0,2 7-24 12 * node /scripts/jd_bxsyj.js >> /scripts/logs/jd_bxsyj.log 2>&1
 #见缝插针
 15 10 * * * node /scripts/jd_jfcz.js >> /scripts/logs/jd_jfcz.log 2>&1
+#逛京东会场
+0 0,18 * * * node /scripts/jd_mall_active.js >> /scripts/logs/jd_mall_active.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
