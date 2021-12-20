d0d3a5def050330f4de93a562db8189eb5dbade1
chiupam <chiupam@126.com>
2021-12-20 16:09:37 +0800

    完成！

--- main.py
+++
@@ -1,7 +0,0 @@
-from requests import post
-
-url = "https://service-7p46ib8s-1304375540.gz.apigw.tencentcs.com/release/genToken"
-data = {"wskey": ""}
-proxies = {"http": None, "https": None}
-res = post(url, data=data, proxies=proxies)
-print(res.text)
