a1592ea2ac9aa4b70a9417a328ed4222586be7bd
chiupam <chiupam@126.com>
2021-12-20 16:01:59 +0800

    更新

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from requests import post
 
-url = "https://service-fp8rk7fs-1304375540.gz.apigw.tencentcs.com/release/genToken"
+url = "https://service-7p46ib8s-1304375540.gz.apigw.tencentcs.com/release/genToken"
 data = {"wskey": ""}
 proxies = {"http": None, "https": None}
 res = post(url, data=data, proxies=proxies)
