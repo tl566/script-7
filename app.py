d639c5221ba3a2256b152f156821316f67ce0861
chiupam <chiupam@126.com>
2021-12-20 16:27:59 +0800

    Have a fun!

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 import flask
 from urllib3 import disable_warnings
+from flask import request
+from uuid import uuid4
+from urllib.parse import quote
+from json import dumps
 
 disable_warnings()
 server = flask.Flask(__name__)
 
 
+def get_sign(functionId, body, uuid, client, clientVersion):
+    sign = functionId + body + uuid + client + clientVersion
+    return sign
+
+
 @server.route('/genToken', methods=['post'])
 def main():
-    return "have a fun!"
+    url = request.values.get('url')
+    if url:
+        body = '{"to":"%s"}' % url
+        sign = get_sign("genToken", body, "".join(str(uuid4()).split("-")), "apple", "10.0.10")
+        res = {"code": 200, "data": {"body": f'body={quote(body)}', "sign": sign}}
+    else:
+        res = {"code": 400, "data": "请传入url参数！"}
+    if url:
+        print(res)
+        return dumps(res, ensure_ascii=False)
 
 
 if __name__ == '__main__':
